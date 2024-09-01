c5af7fafca47e0889a3b91a559a27eccf43545e2
kedarshenoy <akedarshenoy@gmail.com>
2024-09-01 23:20:06 +0530

    backend

--- Logins/Login.js
+++ Logins/Login.js
@@ -6,10 +6,8 @@ router.post("/", async (req, res) => {
   const ref = db.ref('users');
   const { email, Password } = req.body;
 
-  // Query the database to check for existing user with same email and Password
   ref.orderByChild("email").equalTo(email).once("value", snapshot => {
     if (snapshot.exists()) {
-      // Check if any of the matched records also has the same password
       let userExists = false;
       snapshot.forEach(childSnapshot => {
         if (childSnapshot.val().Password === Password) {
@@ -21,7 +19,6 @@ router.post("/", async (req, res) => {
         console.log('User with this email and password already exists');
         res.status(200).send("User with this email and password already exists");
       } else {
-        // If the email exists but the password doesn't match, you can handle it accordingly
         res.status(400).send("Wrong password");
       }
     } else {
